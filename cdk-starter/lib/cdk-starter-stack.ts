e8b5c47d67dc420dde82833654419dfdc81e91e8
tbindewald96 <tobias.bindewald96@gmx.de>
2025-12-21 08:18:23 +0000

    Remove comments; add code

--- cdk-starter/lib/cdk-starter-stack.ts
+++ cdk-starter/lib/cdk-starter-stack.ts
@@ -1,3 +1,4 @@
+import { CfnOutboundExternalLink } from 'aws-cdk-lib/aws-rtbfabric';
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib/core';
 import { Duration } from 'aws-cdk-lib/core';
@@ -30,17 +31,28 @@ export class CdkStarterStack extends cdk.Stack {
         }]
       }
     });
+
+    const duration = new cdk.CfnParameter(this, 'duration', {
+      default: 6,
+      minValue: 1, 
+      maxValue: 10,
+      type: 'Number'
+    }
+    )
     
-    new Bucket(this, 'MyL2Bucket', {
+    const myL2Bucket = new Bucket(this, 'MyL2Bucket', {
       lifecycleRules: [{
-        expiration: Duration.days(2)
+        expiration: Duration.days(duration.valueAsNumber)
       }]
-    })
+    });
+
+    new cdk.CfnOutput(this, 'MyL2BucketName', {
+      value: myL2Bucket.bucketName
+    });
+
+    console.log('bucket name: ' + myL2Bucket.bucketName)
 
     new L3Bucket(this, 'MyL3Bucket', 3);
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStarterQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
   }
 }
