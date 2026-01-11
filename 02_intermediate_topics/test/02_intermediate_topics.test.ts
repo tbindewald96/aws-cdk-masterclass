9add8c1b124bce62a855ec92137a5801fbbe2be8
tbindewald96 <tobias.bindewald96@gmx.de>
2026-01-11 15:41:24 +0000

    initialize environment

---
+++ 02_intermediate_topics/test/02_intermediate_topics.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as 02IntermediateTopics from '../lib/02_intermediate_topics-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/02_intermediate_topics-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new 02IntermediateTopics.02IntermediateTopicsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
